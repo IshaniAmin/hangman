a530b7db6caf3f9689f5cda1c638af23969f2827
Ishani Amin <ishaniamin@Ishanis-MacBook-Pro.local>
2016-11-18 23:50:41 -0500

    added a lot of js changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,39 +1,35 @@
 
-var options = ["batman"]
-// var options = ["batman", "superman", "robin", "wonderwoman", "flash", "arrow"]
+var options = ["batman", "superman", "robin", "wonderwoman", "flash", "arrow"]
 var wins = 0;
 var guesses = [];
-var guessesLeft = 11;
+var guessesLeft = 0;
+var splitWord = [];
+var midWord = []
+var foundWord = []
 
 var display = document.querySelector("#main");
 var randomWord;
 
+function decideSuperhero() {
 
-
-function getStarted() {
-
-    var text = document.querySelector("#secret-word");
-    text.innerHTML = '';
     randomWord = options[Math.floor(Math.random()*options.length)];
-    for (var k=0; k < randomWord.length ; k++ ) {
-        text.innerHTML +=  '_' ;
-    }
-    console.log(randomWord); //checking what the random word is
-}
 
-function playGame (){
-    var display = document.querySelector("#main")
-            text = '<p> Wins: ' + wins + '</p>';
-            text +='<p> Current Word: </p>';
-            
-            text +='<p> Number of guesses left: ' + guessesLeft + '</p>';
-            text += '<p>Your guesses so far: ' + guesses + '</p>';
             if (guessesLeft == 0) {
-                    guessesLeft = 11
+                    guessesLeft = 10;
                     guesses = [];
             }
-            display.innerHTML = text;
+            splitWord = randomWord.split("");
+            console.log(splitWord)
+            for (var i=0;i<splitWord.length;i++) {
+                midWord[i] = '_';
+                foundWord[i] = false;
+            }
+
+            console.log(randomWord);
+            console.log(midWord)
 }
+
+    
 
 
 document.onkeyup = function(event){
@@ -41,32 +37,47 @@ document.onkeyup = function(event){
     var key = event.key;  
     console.log("key chosen", key);
 
+    var match = false;
     //spacebar is keycode 32
     if(event.which == 32){
-        getStarted();
+        decideSuperhero();
+    } 
+    else {
+        for (i=0;i<splitWord.length;i++) {
+            
+            if (splitWord[i] == key) {
+                if (foundWord[i] == false) {
 
-    }   
-
-    if (randomWord) {
-        var splitWord = [];
-        splitWord = randomWord.split("");
-            if (splitWord.indexOf(key) > -1) {
-                playGame();
-    
-            } else if (splitWord.indexOf(key) == -1) {
-                guessesLeft--;
-                guesses.push(key);
-                // console.log(guessesLeft); //checking how many guesses left
-                // console.log(guesses); //checking which guesses were guesses
-                playGame();
+                foundWord[i] = true;
+                midWord[i] = key;
+                wins++;         
+                match = true;
             }
+            }
+        
+                }
+   if (match == false)
+            guessesLeft--;  
+
     }
+    
+        
 
-
-
-
-
-    // console.log(randomWord)
+        if (guessesLeft == 0) {
+            decideSuperhero();
+        }
+    
+     
+            var display = document.querySelector("#main")
+            var text = document.querySelector("#secret-word");
+            text = '<p> Wins: ' + wins + '</p>';
+            text +='<p> Current Word: <br>';
+            text += midWord;
+            text += '</p>';
+            text +='<p> Number of guesses left: ' + guessesLeft + '</p>';
+            text += '<p>Your guesses so far: ' + guesses + '</p>';
+            display.innerHTML = text;
+    
 
     // push the letter chosen (key) into the guesses array
     // compare the letter chosen with the letters in the randomWord
@@ -75,4 +86,3 @@ document.onkeyup = function(event){
     // call decideSuperhero(); when you win to reset randomWord
     // when game resets, you will need to reset the variables (e.g. guesses, gussesLeft etc)
 }
-
